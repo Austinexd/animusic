c272d962775089741bf780aaf837a6fb7f4030b6
austinexd <austinchiao@hotmail.com>
2023-05-16 17:54:21 +0800

    changed base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/animusic/",
   build: {
     rollupOptions: {
       external: ["react-icons/Ai"],
