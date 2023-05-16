6d004cd77d0b870406020c09bc59a7d966462891
austinexd <austinchiao@hotmail.com>
2023-05-17 00:02:50 +0800

    import useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useRef } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
