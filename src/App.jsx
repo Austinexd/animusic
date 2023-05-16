d6afc23261ab5cd5850c9652975fdcecf247eef9
austinexd <austinchiao@hotmail.com>
2023-05-16 23:59:18 +0800

    fixed path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
-import Welcome from "../components/Welcome";
+import Welcome from "./components/Welcome";
 
 function App() {
   const musicRef = useRef();
